b680f96fb96818332a46191d7d53f5adc0b56bbb
dev-pep <79202904+dev-pep@users.noreply.github.com>
2023-02-09 17:13:36 +0100

    RSA

--- README.md
+++ README.md
@@ -52,7 +52,40 @@ El algoritmo genérico utilizado para todos ellos es, dado un mensaje formado po
 
 ### rsa.py
 
-Algoritmo de encriptación de **Rivest, Shamir y Adleman**. Utilidades para calcular y trabajar con el algoritmo de encriptación de clave pública *RSA*. Su utilidad no es para mensajes excesivamente largos. En este caso, se puede utilizar para intercambiar previamente una clave y utilizar un algoritmos de clave asimétrica como *AES*.
+Algoritmo de encriptación de **Rivest, Shamir y Adleman**. Utilidades para calcular y trabajar con el algoritmo de encriptación de clave pública *RSA*. Debido a la lentitud en encriptar (y desencriptar), su utilidad no es para mensajes excesivamente largos. En este caso, se puede utilizar para intercambiar previamente una clave y utilizar un algoritmo de clave asimétrica, mucho más rápido, como es *AES*.
+
+#### Algoritmo
+
+Dos números **enteros** son coprimos entre sí si su mcd (máximo común divisor) es 1, es decir, si no tienen ningún factor común.
+
+Se define la función **totiente de Euler** φ(n) (phi de n) como la cantidad de enteros menores que n que son coprimos de n. Para un número primo n, φ(n)=n-1. Para un número n compuesto por la multiplicación de dos números primos p y q:
+
+φ(n) = φ(p) φ(q) = (p-1) (q-1)
+
+Actualmente dicha función se usa menos, en favor del llamado **totiente de Carmichael**.
+
+#### Método actual
+
+El método actual de obtener las claves se parece mucho al original, pero obtiene valores más eficientes.
+
+Primero, se obtienen dos números primos **grandes**. Originalmente se recomendaba que estos fuesen algo distintos en tamaño. Actualmente es frecuente que ambos tengan el mismo número de bits. Ambos suelen tener sus dos bits más significativos en 1 para asegurar que el número de bits del producto (p*q) tiene tantos bits como la suma del número de bits de cada primo. El número n es el producto de estos dos primos (n=p*q), y será el módulo usado para los cálculos de encriptación y desencriptación; n se comparte como parte de la clave pública (nunca p ni q).
+
+El siguiente paso es calcular el totiente de Carmichael para n: λ(n). Dado que n=p*q, dicho totiente tiene la propiedad λ(n)=mcm(λ(p),λ(q)), siendo mcm el mínimo común múltiplo. Dado que p y q son primos, λ(p)=φ(p)=p−1, y lo mismo con q. Así, λ(n)=mcm(p−1,q−1). Dado que mcm(a,b)=|ab|/mcd(a,b), el mcm se puede calcular usando el **algoritmo de Euclides** (usado para calcular el mcd).
+
+Ahora se calcula el otro número que forma parte de la clave pública: e, tal que 2<e<λ(n). Por otro lado, e y λ(n) son coprimos, es decir, mcd(e,λ(n))=1. Dado que la encriptación utiliza exponenciación binaria, cuanto más corto sea este número y cuantos menos bits 1 contenga, más eficiente será dicha encriptación. Anteriormente se solía utilizar e=3, pero esto le confería menos seguridad al algoritmo. Un número frecuentemente utilizado es 2^16 + 1 (65537), que es un número primo (seguro pues que es coprimo con λ(n)) y con solo dos bits a 1.
+
+Ahora que ya tenemos la clave pública (e y n), calcularemos la clave privada, a la que llamaremos d. Este número es el inverso módulo λ(n) de e. Es decir:
+
+d * e ≡ 1 (mod λ(n))
+
+Para su cálculo, se utiliza el llamado **algoritmo de Euclides extendido**.
+
+Nunca se deben revelar p, q ni λ(n) (se pueden simplemente descartar tras los cálculos). Ni por supuesto d.
+
+Si M es el mensaje en claro (pasado a bits, y representado como entero), menor a n, y C es el mensaje cifrado (también menor a n), para encriptar y desencriptar:
+
+C ≡ M ** e (mod n)
+M ≡ C ** d (mod n)
 
 ### aes.py
 
@@ -62,7 +95,7 @@ Algoritmo de encriptación asimétrica **Advanced Encryption Standard**.
 
 Explora el [Bitcoin Improvement Proposal 39](https://github.com/bitcoin/bips/blob/master/bip-0039.mediawiki), relativo a la *seed phrase*. Utiliza la *wordlist* oficial en inglés, almacenada en el archivo ***wordlist.json***.
 
-## Apéndices
+## Aspectos matemáticos
 
 ### Congruencia módulo n
 
@@ -70,6 +103,14 @@ La congruencia módulo n se indica ***a ≡ b (mod n)***. Es una relación de eq
 
 Dos número enteros ***a***, ***b*** son congruentes módulo ***n*** si ***n*** es divisor de su diferencia:
 
-a ≡ b (mod n) implica que (a-b) ≡ kn, para algún entero ***k***. Esto implica:
+a ≡ b (mod n) implica que (a-b) ≡ kn, para algún entero ***k***. Es lo mismo que decir:
+
+a ≡ kn + b (mod n)
+
+En todo caso:
 
 a-b ≡ 0 (mod n), ya que si a mod n = b mod n, entonces (a-b) mod n es cero.
+
+### Exponenciación módulo n
+
+En ocasiones, como en la encriptación/desencriptación RSA, es necesario calcular potencias con enteros muy grandes en criptografía. Esta operación puede llevar mucho tiempo. Sin embargo, cuando al resultado se le aplica el módulo n, existen métodos muy rápidos para hacer el cálculo. De hecho la función `pow(b, e, n)` utiliza uno de esos métodos de **exponenciación binaria** (*exponentiation by repeated squaring*).
--- rsa.py
+++ rsa.py
@@ -4,16 +4,132 @@
 
 import random
 import utils
+import primos
 
 # Funciones auxiliares *************************************************************************************************
 
+def mcd(x, y):
+    """Calcula el máximo común divisor de dos enteros mediante el algoritmo de Euclides
+
+    :param x: primer entero
+    :param y: segundo entero
+    :return: máximo común divisor de los dos enteros
+    """
+    x1 = max(x, y)
+    x2 = min(x, y)
+    # Hallaremos q y r tal que: x1 = q * x2 + r
+    q = x1 // x2
+    r = x1 - q * x2
+    while r:  # mientras el resto no sea 0
+        lastr = r
+        x1 = x2
+        x2 = r
+        q = x1 // x2
+        r = x1 - q * x2
+    return lastr
+
+def mcm(x, y):
+    """Calcula el mínimo común múltiplo de dos enteros usando el algoritmo de Euclides para encontrar antes el mcd
+
+    :param x: primer entero
+    :param y: segundo entero
+    :return: mínimo común múltiplo de los dos enteros
+    """
+    return abs(x * y) // mcd(x, y)
+
+def euclid_ext(x, y):
+    """Calcula el mcd y los factores de Bezout de dos enteros mediante el algoritmo extendido de Euclides
+
+    :param x: primer entero
+    :param y: segundo entero
+    :return: máximo común divisor de los dos enteros, así como los coeficientes de Bezout (mcd, s, t)
+    """
+    # Buscaremos el mcd, así como los coeficientes s y t de la identidad de Bezout: mcd = s*x + t*y.
+    # Para ello usaremos 4 series de datos:
+    q = [None, max(x, y)]  # cocientes
+    r = [max(x, y), min(x, y)]  # restos
+    s = [1, 0]  # coeficientes s
+    t = [0, 1]  # coeficientes t
+    while r[-1]:  # mientras el último elemento de r no sea 0
+        q.append(r[-2] // r[-1])
+        r.append(r[-2] - q[-1] * r[-1])
+        s.append(s[-2] - q[-1] * s[-1])
+        t.append(t[-2] - q[-1] * t[-1])
+    # Ya tenemos el resultado. Si lo usamos para encontrar el inverso módulo n, es decir, un si buscamos
+    # un número N tal que y * N mod x = 1, la respuesta es t[-2] mod x
+    return r[-2], s[-2], t[-2]
+
+def inverso_modn(x, n):
+    """Calcula el inverso módulo n del entero x
+
+    Utiliza el algoritmo de Euclides extendido, euclid_ext().
+    :param x: entero del que calcular el inverso módulo n
+    :param n: módulo para el cálculo
+    :return: el inverso módulo n calculado
+    """
+    # El resultado será un número y tal que (x * y) % n = 1
+    assert(n > x)
+    inv = euclid_ext(n, x)[2] % n
+    try:
+        assert(x * inv % n == 1)
+    except AssertionError:
+        # x solo tiene inverso módulo n si x y n son coprimos
+        return None
+    return inv
+
+def rnd_primo(nbits=1024, k=10):
+    """Obtiene un número primo aleatorio de la cantidad de bits especificada (útil para p y q)
+
+    :param nbits: número de bits del primo
+    :param k: número de iteraciones Miller-Rabin
+    :return: el primo aleatorio
+    """
+    es_primo = False
+    while not es_primo:
+        # Obtendremos un número primo siguiendo recomendaciones habituales. En primer lugar, se recomiendan 1024 bits.
+        entero = random.randint(0, 2 ** nbits - 1)
+        # Colocamos el primer bit a 1 (si fuese un 0 ya no sería un entero de nbits):
+        entero |= 2 ** (nbits - 1)
+        # Ahora colocamos el segundo bit también a 1: es la única forma de asegurar que dos números primos de n bits
+        # multiplicados produzcan un número de n*2 bits (y no n*2 - 1), lo cual se recomienda frecuentemente:
+        entero |= 2 ** (nbits - 2)
+        # También colocamos el último bit a 1, para asegurarnos de que es un entero impar:
+        entero |= 1
+        es_primo = primos.es_primo_mr(entero, k)  # k=10 iteraciones parecen ser más que suficientes
+    return entero
+
+def genera_claves_rsa(nbits, k):
+    # Primero, obtenemos los números primos p y q (y n):
+    p = rnd_primo(nbits, k)
+    q = rnd_primo(nbits, k)
+    n = p * q
+    # Ahora, calculamos λ(n) (Carmichael's totient):
+    car_toti = mcm(p - 1, q - 1)
+    # Valor típico para e:
+    e = 65537
+    assert (e < car_toti)
+    # Ya solo queda calcular la clave privada (d), mediante el algoritmo de Euclides extendido.
+    # Se debe dar e * d = 1 (mod car_toti):
+    d = inverso_modn(e, car_toti)
+    return n, e, d
+
 # Funciones de las opciones de menú ************************************************************************************
+
+def menu_genera_claves():
+    """Genera el par de claves RSA"""
+    nbits = utils.input_int("Número de bits de p y q (512-4096, por defecto 1024)", range(512, 4097), 1024)
+    k = utils.input_int("Número de iteraciones para el cálculo de p y q (2-128, por defecto 10)", range(2, 129), 10)
+    n, e, d = genera_claves_rsa(nbits, k)
+    print("Clave pública:")
+    print("e:", e)
+    print("n:", n)
+    print("Clave privada:")
+    print("d:", d)
 
 # Menu *****************************************************************************************************************
 
 opciones_menu = (
-    ("Primera opción de menú", None),
-    ("Segunda opción de menú", None)
+    ("Genera par de claves", menu_genera_claves),
 )
 
 # Programa *************************************************************************************************************
